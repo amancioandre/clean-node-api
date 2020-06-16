daff8885b95749f125f94ed4136807a57cb2e353
André Moraes <amancioandre@gmail.com>
2020-06-16 11:18:55 -0300

    refactor: add SutTypes interface and change makeSut

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -2,13 +2,21 @@ import { badRequest } from '../../helpers/http-helper'
 import { MissingParamError } from '../../errors'
 import { LoginController } from './login'
 
-const makeSut = (): LoginController => {
-  return new LoginController()
+interface SutTypes {
+  sut: LoginController
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new LoginController()
+
+  return {
+    sut
+  }
 }
 
 describe('Login Controller', () => {
   test('Should return 400 if no email is provided.', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         password: 'any_password'
@@ -19,7 +27,7 @@ describe('Login Controller', () => {
   })
 
   test('Should return 400 if no password is provided.', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         email: 'any_email@mail.com'
