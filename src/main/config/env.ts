53583921549316e96459aa2042e548b4e9323545
André Moraes <amancioandre@gmail.com>
2020-06-22 09:36:22 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoURI: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
-  port: process.env.PORT || 5050
+  port: process.env.PORT || 5050,
+  jwtSecret: process.env.SECRET_KEY || 'supersecret'
 }
